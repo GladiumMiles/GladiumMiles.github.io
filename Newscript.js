945f66ac18241e3c4dd800d132475f0be6b6ea62
GladiumMiles <stoutland99@gmail.com>
2018-04-07 19:13:47 +0200

    Nya sidor
    Lagt till lite nytt

--- Newscript.js
+++ Newscript.js
@@ -1,14 +1,19 @@
 // JavaScript Document
 
-
-var keys = [".key1, .key2, .key3, .key4, .key5, .key6, .key7, .key8, .key9, .key10"]
-keys = 0;
-
-
-if (.key1 % 2 == 1) {} else {} 
+var namn 
 
 
 
+namn =prompt("Mata in in ditt namn");
+
+bokstav1 = namn.charAt(0);
+
+
+if (namn.charAt(0) = A ) {document.write(bokstav1)} else {};
+
+
+
+document.write(bokstav1);
 
 
 
